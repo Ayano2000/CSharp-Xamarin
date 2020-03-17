4ddb6e02cfaee25e66e67e933773cddf4779994a
Arata Yano <aratayano00@gmail.com>
2020-03-17 12:55:00 +0200

    changed location service so it returns just the city name

--- PrismApp/PrismApp/PrismApp/Services/ILocationService.cs
+++ PrismApp/PrismApp/PrismApp/Services/ILocationService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PrismApp.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,6 @@ namespace PrismApp.Services
 {
     public interface ILocationService
     {
-        Task<string> GenerateQuery();
+        Task<string> GetLocation();
     }
 }
--- PrismApp/PrismApp/PrismApp/Services/LocationService.cs
+++ PrismApp/PrismApp/PrismApp/Services/LocationService.cs
@@ -1,19 +1,39 @@
-﻿using System.Threading.Tasks;
+﻿using PrismApp.ViewModels;
+using System.Threading.Tasks;
 using Xamarin.Essentials;
+using System;
 
 namespace PrismApp.Services
 {
     class LocationService : ILocationService
     {
-        public async Task<string> GenerateQuery()
+        private IRestService _restService;
+        private IQueryService _queryService;
+
+        public LocationService(IRestService restService, IQueryService queryService)
+        {
+            _restService = restService;
+            _queryService = queryService;
+        }
+
+        //public async Task<string> GenerateQuery()
+        //{
+        //    var request = new GeolocationRequest(GeolocationAccuracy.Best);
+        //    var location = await Geolocation.GetLocationAsync(request);
+        //    string query = Constants.Constants.Endpoint;
+        //    query += $"?lat={location.Latitude}&lon={location.Longitude}";
+        //    query += "&units=metric"; // or units=imperial
+        //    query += $"&APPID={Constants.Constants.APIKey}";
+        //    return (query);
+        //}
+        public async Task<string> GetLocation()
         {
             var request = new GeolocationRequest(GeolocationAccuracy.Best);
             var location = await Geolocation.GetLocationAsync(request);
-            string query = Constants.Constants.Endpoint;
-            query += $"?lat={location.Latitude}&lon={location.Longitude}";
-            query += "&units=metric"; // or units=imperial
-            query += $"&APPID={Constants.Constants.APIKey}";
-            return (query);
+            // Long then Lat not other way around, if only that didnt take 20 minutes
+            string query = _queryService.GenerateQuery(location.Longitude, location.Latitude);
+            var city = await _restService.GetWeatherData(query);
+            return (city.Title);
         }
     }
 }
