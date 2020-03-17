ab7312e400100bd1aade0706086b75d9a55e2694
charlie <charles@bbd.co.za>
2020-03-17 10:35:31 +0200

    - added test project
    added a bunch of Rider specific stuff to the gitignore as well

---
+++ PrismApp/PrismApp.Tests/TestBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace PrismApp.Tests
+{
+    [TestFixture]
+    public class TestBase
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.That(true, Is.False); //fails, expects false
+        }
+    }
+}
