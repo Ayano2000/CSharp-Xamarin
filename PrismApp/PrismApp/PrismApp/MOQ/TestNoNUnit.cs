6f85cdebd6c0d529ba3eefc26a9c2a0a18e3b449
Arata Yano <aratayano00@gmail.com>
2020-03-17 10:11:57 +0200

    added another attempt at running tests through VS

---
+++ PrismApp/PrismApp/PrismApp/MOQ/TestNoNUnit.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using PrismApp.Services;
+using System;
+
+namespace PrismApp.MOQ
+{
+    [TestClass]
+    public class TestNoNUnit
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            Assert.AreEqual(true, false);
+        }
+    }
+}
