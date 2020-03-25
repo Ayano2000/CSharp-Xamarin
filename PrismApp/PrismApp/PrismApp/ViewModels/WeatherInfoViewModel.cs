82dfd4f3f29144783464e2bfac0baf423dc1a87d
Arata Yano <aratayano00@gmail.com>
2020-03-25 08:21:53 +0200

    removed button from GetWeatherInfo page so the command runs from the constructor

--- PrismApp/PrismApp/PrismApp/ViewModels/WeatherInfoViewModel.cs
+++ PrismApp/PrismApp/PrismApp/ViewModels/WeatherInfoViewModel.cs
@@ -30,6 +30,7 @@ namespace PrismApp.ViewModels
             _navigationService = navigationService;
             _queryService = queryService;
             GetWeatherCommand = new Command(async () => await GetWeatherInfo());
+            GetWeatherCommand.Execute(null);
         }
         public ObservableCollection<CityWeatherViewModel> CityWeatherViewModels
         {
